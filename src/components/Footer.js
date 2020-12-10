7f84c93adb85ec969719c7feb270472c10817228
Augustin Berisa <burim.berisa@protonmail.com>
2020-12-10 07:50:44 +0100

    Boldy footer stack

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,9 @@ export const Footer = (props) => {
 				   m='auto 0'
 				   flexGrow='1'
 				   justifyContent='center'
-				   fontSize={{ base: '0.55rem', sm: 'sm' }}>
+				   fontSize={{ base: '0.55rem', sm: 'sm' }}
+				   fontWeight='600'
+			>
 				<a href='https://www.emptyset.finance/' target='_blank' rel='noreferrer'>Site</a>
 				<a href='https://docs.emptyset.finance' target='_blank' rel='noreferrer'>Documentation</a>
 				<a href='https://docs.emptyset.finance/faqs/basics' target='_blank' rel='noreferrer'>FAQs</a>
