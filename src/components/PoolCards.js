bfbe3fbcc5dc4fcdbd8a28b9fd2309bf3ae5ae62
Augustin Berisa <burim.berisa@protonmail.com>
2020-12-11 02:23:31 +0100

    Add fade in transition

--- src/components/PoolCards.js
+++ src/components/PoolCards.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import axios from 'axios'
 import {
-	Heading, Grid, Flex, Stat, StatLabel, StatNumber, Tag, Button, useBreakpointValue,
+	Heading, Grid, Fade, Flex, Stat, StatLabel, StatNumber, Tag, Button, useBreakpointValue,
 	useColorModeValue,
 } from '@chakra-ui/react'
 import { prettifyCurrency } from '../common/utils'
@@ -46,39 +46,39 @@ export const PoolCards = (props) => {
 	}, [])
 
 	return (
-		<Grid
-			minHeight='435px'
-			m='0px auto 1.2rem auto'
-			templateColumns={templateColumns}
-			gap={{ base: 36.5, sm: 84 }}
-		>
+		<>
 			{!loading &&
-				<>
-					{pools.data.map((pool, index) => {
-						return (
-							<Flex key={index}
+				<Fade in={true}>
+					<Grid
+						minHeight='435px'
+						m='0px auto 1.2rem auto'
+						templateColumns={templateColumns}
+						gap={{ base: 36.5, sm: 84 }}
+					>					{pools.data.map((pool, index) => {
+							return (
+								<Flex key={index}
 								  flexDirection='column'
 								  borderRadius='43px'
 								  p={cardPadding}
 								  bg={background}
 								  style={{ cursor: 'pointer' }}
-							>
-								<Heading textStyle='h2'
+								>
+									<Heading textStyle='h2'
 										 size='lg'
 										 color={color}>
-									{pool.name}
-								</Heading>
-								<Stat>
-									<StatLabel textStyle='body'
+										{pool.name}
+									</Heading>
+									<Stat>
+										<StatLabel textStyle='body'
 											   color={color}
-									>Total value locked</StatLabel>
-									<StatNumber textStyle='body'
-										color={color}
-										fontSize={{ base: 'xs', sm: 'lg', lg: 'xl' }}>
-										{prettifyCurrency(pool.totalValueLockedInUSD)}
-									</StatNumber>
-								</Stat>
-								<Tag size='md'
+										>Total value locked</StatLabel>
+										<StatNumber textStyle='body'
+											color={color}
+											fontSize={{ base: 'xs', sm: 'lg', lg: 'xl' }}>
+											{prettifyCurrency(pool.totalValueLockedInUSD)}
+										</StatNumber>
+									</Stat>
+									<Tag size='md'
 									 fontSize='1.1rem'
 									 fontWeight='600'
 									 justifyContent='flex-end'
@@ -86,21 +86,22 @@ export const PoolCards = (props) => {
 									 bg='transparent'
 									 p='0'
 									 color={color}
-								>
-									<Button bg='transparent'
-										_hover={{ bg: 'transparent' }}
-										color={color}
-										fontSize='1.2rem'
-										fontWeight='600'
 									>
+										<Button bg='transparent'
+											_hover={{ bg: 'transparent' }}
+											color={color}
+											fontSize='1.2rem'
+											fontWeight='600'
+										>
 										earn<span style={{ marginLeft: '7px' }}>⟶</span>
-									</Button>
-								</Tag>
-							</Flex>
-						)
-					})}
-				</>
+										</Button>
+									</Tag>
+								</Flex>
+							)
+						})}
+					</Grid>
+				</Fade>
 			}
-		</Grid>
+		</>
 	)
 }
