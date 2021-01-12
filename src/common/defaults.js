a1c4b923c1892742401578b36f4ca8731f0dd9a2
Augustin Berisa <burim.berisa@protonmail.com>
2021-01-12 09:48:00 +0100

    Get DAO implementation util

--- src/common/defaults.js
+++ src/common/defaults.js
@@ -10,7 +10,7 @@ defaults.network.connectors = undefined
 
 defaults.contracts = {}
 defaults.contracts.esd = '0x36F3FD68E7325a35EB768F1AedaAe9EA0689d723'
-defaults.contracts.esds = '0xf7752941CB46785a51b23859DeB36fdf1e18aF4d'
+defaults.contracts.root = '0x443D2f2755DB5942601fa062Cc248aAA153313D3'
 defaults.contracts.usdc = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48'
 
 defaults.toast = {}
--- src/common/utils.js
+++ src/common/utils.js
@@ -1,3 +1,5 @@
+import { ethers } from 'ethers'
+
 const prettifyAddress = (address) => {
 	return `${address.substring(0, 7)}...${address.substring(address.length - 4, address.length)}`
 }
@@ -51,11 +53,12 @@ const getPoolLocation = (collateralA = '', collateralB = '') => {
 }
 
 const getDaoImplementation = async (address, provider) => {
-	return await provider.getStorageAt(
-		address,
-		'0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc',
-		// bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1)
-	)
+	return ethers.utils.hexStripZeros(
+		await provider.getStorageAt(
+			address,
+			'0x360894a13ba1a3210667c828492db98dca3e2076cc3735a920a3ca505d382bbc',
+			// bytes32(uint256(keccak256('eip1967.proxy.implementation')) - 1)
+		))
 }
 
 export { prettifyAddress, prettifyCurrency, prettifyNumber, getPercentage, getPoolLocation, getDaoImplementation }
